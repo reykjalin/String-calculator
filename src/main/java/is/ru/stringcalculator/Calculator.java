027506f164a83fce38dda90d7966a11896592f42
Kristofer Reykjalin Thorlaksson <kristofer@i-1217c9a8.cs1004cloud.internal>
2016-10-14 16:10:15 +0000

    Add negative test, passed

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,11 +1,25 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-    public static int add(String str){
+    public static int add(String str) throws Exception {
         if(str.isEmpty())
             return 0;
 
         String nums[] = splitString(str);
+        boolean first = true;
+        String msg = "";
+        for(int i = 0; i < nums.length; i++){
+            if(toInt(nums[i]) < 0){
+                if(first){
+                    msg += "Negatives not allowed: " + nums[i];
+                    first = false;
+                }
+                else
+                    msg += "," + nums[i];
+            }
+        }
+        if(!msg.isEmpty())
+            throw new Exception(msg);
         return sum(nums);
     }
 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,28 +6,38 @@ import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
 public class CalculatorTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void emptyTest(){
+    public void emptyTest() throws Exception {
         assertEquals(0, Calculator.add(""));
     }
 
     @Test
-    public void oneNumTesT(){
+    public void oneNumTesT() throws Exception {
         assertEquals(1, Calculator.add("1"));
     }
 
     @Test
-    public void twoNumTest(){
+    public void twoNumTest() throws Exception {
         assertEquals(3, Calculator.add("1,2"));
     }
 
     @Test
-    public void multNumTest(){
+    public void multNumTest() throws Exception {
         assertEquals(45, Calculator.add("1,2,3,4,5,6,7,8,9"));
     }
 
     @Test
-    public void nlTest(){
+    public void nlTest() throws Exception {
         assertEquals(6, Calculator.add("1,2\n3"));
+    }
+
+    @Test
+    public void exTest() throws Exception {
+        thrown.expect(Exception.class);
+        thrown.expectMessage("Negatives not allowed: -1,-3");
+        Calculator.add("1,-1\n-3");
     }
 }

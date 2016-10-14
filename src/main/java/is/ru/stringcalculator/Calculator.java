ef3b4cb595bc11762595700127c8c6f10d28321c
Kristofer Reykjalin Thorlaksson <kristofer@i-1217c9a8.cs1004cloud.internal>
2016-10-14 01:54:26 +0000

    Kata finished in 24 mins
    Previous kata finished in 1h 30m, mainly due to information
    gathering for JUnit

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,4 +1,64 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
+    public static int add(String str) throws Exception {
+        if(str.isEmpty())
+            return 0;
+        else{
+            String delim = getDelim(str);
+            if(!delim.isEmpty())
+                str = removeDelim(str);
+
+            String nums[] = splitString(str, delim);
+            String msg = findNeg(nums);
+            if(msg.isEmpty())
+                return getSum(nums);
+            throw new Exception(msg);
+        }
+    }
+
+    private static int toInt(String str) { return Integer.parseInt(str); }
+
+    private static int getSum(String[] nums){
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int num = toInt(nums[i]);
+            if(num <= 1000)
+                sum += num;
+        }
+        return sum;
+    }
+
+    private static String[] splitString(String str, String delim){
+        return str.split("[,\\n" + delim + "]");
+    }
+    
+    private static String findNeg(String[] nums){
+        String msg = "Negatives not allowed: ";
+        boolean hasNeg = false;
+
+        for(int i = 0; i < nums.length; i++){
+            if(isNeg(nums[i])){
+                msg += nums[i] + ",";
+                hasNeg = true;
+            }
+        }
+
+        if(hasNeg)
+            return msg.substring(0, msg.length() - 1);
+        return "";
+    }
+
+    private static boolean isNeg(String str){ return toInt(str) < 0; }
+
+    private static String getDelim(String str){
+        if(str.startsWith("//")){
+            return str.substring(2, str.indexOf("\n"));
+        }
+        return "";
+    }
+
+    private static String removeDelim(String str){
+        return str.substring(str.indexOf("\n") + 1, str.length());
+    }
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,4 +6,48 @@ import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
 public class CalculatorTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void emptyTest() throws Exception {
+        assertEquals(0, Calculator.add(""));
+    }
+
+    @Test
+    public void oneNumTest() throws Exception {
+        assertEquals(1, Calculator.add("1"));
+    }
+
+    @Test
+    public void twoNumTest() throws Exception {
+        assertEquals(3, Calculator.add("1,2"));
+    }
+
+    @Test
+    public void multNumTest() throws Exception {
+        assertEquals(45, Calculator.add("1,2,3,4,5,6,7,8,9"));
+    }
+
+    @Test
+    public void newLineTest() throws Exception {
+        assertEquals(6, Calculator.add("1,2\n3"));
+    }
+
+    @Test
+    public void exceptionTest() throws Exception {
+        thrown.expect(Exception.class);
+        thrown.expectMessage("Negatives not allowed: -1,-3");
+        Calculator.add("-1,2,-3,4,5");
+    }
+
+    @Test
+    public void thousandTest() throws Exception {
+        assertEquals(3, Calculator.add("1,2,100101"));
+    }
+
+    @Test
+    public void customDelimTest() throws Exception {
+        assertEquals(6, Calculator.add("//;\n1;2;3"));
+    }
 }
